b6aa6d1b72b5a0cc80717c40143045fe79102b46
MINJO KIM <114234223+Mongjo@users.noreply.github.com>
2025-06-20 16:28:21 +0900

    Update SwaggerConfig.java

--- src/main/java/com/chalnakchalnak/chatservice/common/config/SwaggerConfig.java
+++ src/main/java/com/chalnakchalnak/chatservice/common/config/SwaggerConfig.java
@@ -29,7 +29,7 @@ public class SwaggerConfig {
         return new OpenAPI()
                 .addSecurityItem(securityRequirement)
                 .components(components)
-                .addServersItem(new Server().url(""))
+                .addServersItem(new Server().url("/chat-service"))
                 .info(apiInfo());
     }
 
